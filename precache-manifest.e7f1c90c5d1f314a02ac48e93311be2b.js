b959d49eea799c042d053fa3ad33f3d9e479d810
ivanzapatarivera <falenyo@gmail.com>
2020-09-24 00:12:45 -0400

    Updates

---
+++ precache-manifest.e7f1c90c5d1f314a02ac48e93311be2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd42971a50932324e327a1b358b584ae",
+    "url": "/react-app-portfolio/index.html"
+  },
+  {
+    "revision": "8074e98b70dfe7e0bcd4",
+    "url": "/react-app-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f679f323f0ce59a6ff1d",
+    "url": "/react-app-portfolio/static/css/main.16a06aaa.chunk.css"
+  },
+  {
+    "revision": "8074e98b70dfe7e0bcd4",
+    "url": "/react-app-portfolio/static/js/2.39f89814.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-portfolio/static/js/2.39f89814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f679f323f0ce59a6ff1d",
+    "url": "/react-app-portfolio/static/js/main.d925bfc6.chunk.js"
+  },
+  {
+    "revision": "0d31de1027b6ceab92df",
+    "url": "/react-app-portfolio/static/js/runtime-main.42ef9427.js"
+  }
+]);
